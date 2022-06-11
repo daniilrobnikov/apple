af727f51ae7b488f594e8d2ca96f5a21778b4547
Daniil Robnikov <daniilrobnikov@gmail.com>
2022-06-11 11:14:59 +0300

    SEO Build 5

--- components/templates/layout/nav/PaddleNav.jsx
+++ components/templates/layout/nav/PaddleNav.jsx
@@ -16,7 +16,7 @@ export default function PaddleNav({
         <svg
           stroke='currentColor'
           fill='currentColor'
-          stroke-width='0'
+          strokeWidth='0'
           viewBox='0 0 512 512'
           height='1em'
           width='1em'
@@ -36,7 +36,7 @@ export default function PaddleNav({
         <svg
           stroke='currentColor'
           fill='currentColor'
-          stroke-width='0'
+          strokeWidth='0'
           viewBox='0 0 512 512'
           height='1em'
           width='1em'
